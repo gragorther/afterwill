6f0780fe9dd95297058fdb5ed567e24cecf6afc1
gregor <gregor@gregtech.eu>
2025-11-03 20:43:39 +0100

    remove user id field from recipients in migration

--- priv/repo/migrations/20251103191812_create_recipients.exs
+++ priv/repo/migrations/20251103191812_create_recipients.exs
@@ -5,7 +5,6 @@ defmodule Afterwill.Repo.Migrations.CreateRecipients do
     create table(:recipients) do
       add :email, :string
       add :last_message, references(:last_messages, on_delete: :nothing)
-      add :user_id, references(:users, type: :id, on_delete: :delete_all)
 
       timestamps(type: :utc_datetime)
     end
